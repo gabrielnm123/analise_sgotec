804f7bb4972bf9cd5f2b5e9858c61256b71b5d6e
Gabriel Nunes <gabrielnmatos96@gmail.com>
2026-02-18 15:47:59 -0300

    uma hora pra executar

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
 from packages.sgotec import sgotec
 from packages.analise import analise
+from time import sleep
 
 if __name__ == '__main__':
     while True:
         sgotec()
         analise()
+        sleep(3600)
